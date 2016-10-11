788d083fc24e833c0ebe7d387a4a229a008a7eef
dizzy <dizzy.lukas@gmail.com>
2016-10-11 19:05:31 +0200

    Add simple  git check

--- set_my_dot_files.sh
+++ set_my_dot_files.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
 #Download actual version form git into ~/temp
-exec git clone https://github.com/Dizzy-cz/DotFiles ~/temp
+if [ -x /usr/bin/git ];then
+    exec git clone https://github.com/Dizzy-cz/DotFiles ~/temp
+else
+    echo "ERROR: Please install git"
+    #failed()
+fi
