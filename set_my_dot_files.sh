70b4b01396122e43612b8e001ad94f4e030a06d5
dizzy <dizzy.lukas@gmail.com>
2016-10-12 14:41:15 +0200

    Added basic code for copy files

--- set_my_dot_files.sh
+++ set_my_dot_files.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Variable
+tempDir=~/temp
+
 # Functions
 failed()
 {
@@ -7,16 +10,38 @@ failed()
     return 1
 }
 
-# Download actual version form git into ~/temp
+# Download actual version form git into $tempDir
 if [ -x /usr/bin/git ]; then
 
-    if [ -d ~/temp ]; then
+    if [ -d $tempDir ]; then
 	echo "WARNING: Temp directory exist, this script rename the old"
-	mv -f ~/temp ~/temp.old
+	
+	if [ -d $tempDir.old ]; then
+	    echo "WARNING: Temp.old directory exist, remove the old"
+	    rm -rf $tempDir.old
+	fi
+	
+	mv -f $tempDir $tempDir.old
     fi
 
-    exec git clone https://github.com/Dizzy-cz/DotFiles ~/temp
+    git clone https://github.com/Dizzy-cz/DotFiles $tempDir
 else
     echo "ERROR: Please install git"
     failed
 fi
+
+
+# Get list of config files
+configFiles=$(ls -A $tempDir |grep -v '.git\|README.md')
+
+# Copy config files to right path
+for file in $configFiles; do
+    echo "Processing file: $file"
+
+    if [ -e ~/$file ]; then
+	    echo "WARNING: $file exist, this script rename the old"
+	    mv ~/$file ~/$file.old
+    fi
+    
+    cp $tempDir/$file ~/
+done
