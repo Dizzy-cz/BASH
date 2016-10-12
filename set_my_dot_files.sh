8675df507be07dbee9f40cf9d4e61c7a189d0d6b
dizzy <dizzy.lukas@gmail.com>
2016-10-12 12:32:40 +0200

    added code for rename temp directory

--- set_my_dot_files.sh
+++ set_my_dot_files.sh
@@ -1,9 +1,22 @@
 #!/bin/bash
 
-#Download actual version form git into ~/temp
-if [ -x /usr/bin/git ];then
+# Functions
+failed()
+{
+    echo "ERROR: Something was wrong!"
+    return 1
+}
+
+# Download actual version form git into ~/temp
+if [ -x /usr/bin/git ]; then
+
+    if [ -d ~/temp ]; then
+	echo "WARNING: Temp directory exist, this script rename the old"
+	mv -f ~/temp ~/temp.old
+    fi
+    
     exec git clone https://github.com/Dizzy-cz/DotFiles ~/temp
 else
     echo "ERROR: Please install git"
-    #failed()
+    failed
 fi
